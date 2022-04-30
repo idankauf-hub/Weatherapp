2e8f66a9f5439c10cdc7b791d876a3430ed74b08
idankauf <idankauf@gmail.com>
2022-04-30 22:37:12 +0300

    Card added

--- src/Comps/card/Card.jsx
+++ src/Comps/card/Card.jsx
@@ -1,5 +1,45 @@
 import React from "react";
-import './card.scss'
+import "./card.scss";
+import ReactAnimatedWeather from "react-animated-weather";
+
 export default function Card() {
-  return <div className="card">card</div>;
+    const defaults = {
+        icon: 'CLEAR_DAY',
+        color: 'goldenrod',
+        size: 80,
+        animate: true
+      };
+  return (
+    // <div className="card">
+    //   <div className="img">
+        // <ReactAnimatedWeather
+        //   icon={defaults.icon}
+        //   color={defaults.color}
+        //   size={defaults.size}
+        //   animate={defaults.animate}
+          
+        // />
+    //   </div>
+    //   <div className="cardInfo">Info</div>
+    // </div>
+    <div className="ForecastDay">
+    <div className="WeatherForecastDay">{"Sat"}</div>
+    <div className="WeatherForecastIcon">
+    <ReactAnimatedWeather
+          icon={defaults.icon}
+          color={defaults.color}
+          size={defaults.size}
+          animate={defaults.animate}
+          
+        />    </div>
+    <div className="WeatherForecastTemp">
+      <span className="WeatherForecastTemp-min">{13}</span>🌡
+      <span className="WeatherForecastTemp-max">{14}</span>
+    </div>
+
+    <div className="WeatherForecastDescription">
+      {"sdsd"}
+    </div>
+  </div>
+  );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Navbar from "../Comps/navbar/Navbar";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import IconButton from '@mui/material/IconButton';
+import IconButton from "@mui/material/IconButton";
 
 import SearchIcon from "@mui/icons-material/Search";
 import Tooltip from "@mui/material/Tooltip";
 
 import "./home.scss";
-import Card from '../Comps/card/Card.jsx'
+import Card from "../Comps/card/Card.jsx";
 export default function Home() {
   return (
     <div className="home">
@@ -18,13 +18,17 @@ export default function Home() {
           id="outlined-basic"
           variant="outlined"
           label="Search"
-          sx={{ backgroundColor:"white",borderBlock:"black" ,width:380}}
+          sx={{ backgroundColor: "white", borderBlock: "black", width: 380 }}
         />
-      <IconButton color="secondary" size="small">
-        <SearchIcon/>
-      </IconButton>
+        <IconButton color="secondary" size="small">
+          <SearchIcon />
+        </IconButton>
       </div>
-      <Card/>
+      <div className="Cards">
+        {[...Array(5)].map((e, i) => (
+          <Card />
+        ))}
+      </div>
     </div>
   );
 }
